9333b7e4f0e6f4fe0ef268d06cdb05fd27ed0ae6
Travis CI User <travis@example.org>
2019-02-25 17:41:13 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,15 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['as_slice'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['byteorder'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['cast'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['generic_array'] = {"name":"","dirs":[],"files":["arr.rs","functional.rs","hex.rs","impls.rs","iter.rs","lib.rs","sequence.rs"]};
+sourcesIndex['hash32'] = {"name":"","dirs":[],"files":["fnv.rs","lib.rs","murmur3.rs"]};
+sourcesIndex['hash32_derive'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['jnet'] = {"name":"","dirs":[{"name":"sixlowpan","dirs":[],"files":["iphc.rs","nhc.rs"]}],"files":["arp.rs","coap.rs","ether.rs","fmt.rs","icmp.rs","icmpv6.rs","ieee802154.rs","ipv4.rs","ipv6.rs","lib.rs","mac.rs","macros.rs","sealed.rs","sixlowpan.rs","traits.rs","udp.rs"]};
+sourcesIndex['owning_slice'] = {"name":"","dirs":[],"files":["from.rs","lib.rs","sealed.rs","to.rs","traits.rs"]};
+sourcesIndex['proc_macro2'] = {"name":"","dirs":[],"files":["lib.rs","stable.rs","strnom.rs"]};
+sourcesIndex['quote'] = {"name":"","dirs":[],"files":["lib.rs","to_tokens.rs","tokens.rs"]};
+sourcesIndex['stable_deref_trait'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['syn'] = {"name":"","dirs":[{"name":"gen","dirs":[],"files":["gen_helper.rs"]}],"files":["attr.rs","buffer.rs","data.rs","derive.rs","error.rs","expr.rs","generics.rs","ident.rs","lib.rs","lifetime.rs","lit.rs","mac.rs","macros.rs","op.rs","parse_quote.rs","parsers.rs","path.rs","punctuated.rs","spanned.rs","synom.rs","token.rs","tt.rs","ty.rs"]};
+sourcesIndex['typenum'] = {"name":"","dirs":[],"files":["array.rs","bit.rs","int.rs","lib.rs","marker_traits.rs","operator_aliases.rs","private.rs","type_operators.rs","uint.rs"]};
+sourcesIndex['unicode_xid'] = {"name":"","dirs":[],"files":["lib.rs","tables.rs"]};
